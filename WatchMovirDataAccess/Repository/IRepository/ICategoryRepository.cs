03c13a27a89d10616d1f24f58e848353be72963f
Bibek Parajuli <parajulibbek11@gmail.com>
2023-03-21 08:55:55 +0545

    CategoryRepoInterface

---
+++ WatchMovirDataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatchMovie.Models;
+
+namespace WacthMovie.DataAccess.Repository.IRepository
+{
+    //this single handles the category models
+    //note: models can be many and sabai model ko update op xuttai huna sakxa testo
+    //case ma chai we perform manually
+
+    public interface ICategoryRepository: IRepository<Category>
+    {
+        void Update(Category obj);
+
+        //save must be done in Repository ma but this is not the good practice 
+        void Save();
+    }
+}
