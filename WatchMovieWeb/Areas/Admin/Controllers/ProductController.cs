c858b48d1d8bac317f601dba31f45bf202fd9f56
Bibek Parajuli <parajulibbek11@gmail.com>
2023-03-27 21:26:01 +0545

    update product

--- WatchMovieWeb/Areas/Admin/Controllers/ProductController.cs
+++ WatchMovieWeb/Areas/Admin/Controllers/ProductController.cs
@@ -90,12 +90,33 @@ namespace WatchMovieWeb.Areas.Admin.Controllers
                     var uploads = Path.Combine(wwwRootPath, @"images\products");
                     var extention = Path.GetExtension(file.FileName);
 
+                    if(obj.Product.ImageUrl != null)
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extention), FileMode.Create))
                     {
                         file.CopyTo(fileStreams);
                     }
                     obj.Product.ImageUrl = @"\images\products\" + fileName + extention;
                 }
+                if(obj.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(obj.Product);
+
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(obj.Product);
+
+
+                }
+
                 _unitOfWork.Product.Add(obj.Product);   //
                 //for creating firstproduct
                 _unitOfWork.Save();        //    
