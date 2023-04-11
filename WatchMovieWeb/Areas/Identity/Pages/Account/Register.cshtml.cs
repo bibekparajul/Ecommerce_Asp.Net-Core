2393d5d976c61c5edac97382f28e04194004e795
Bibek Parajuli <parajulibbek11@gmail.com>
2023-04-11 15:07:51 +0545

    dropDOwnRole

--- WatchMovieWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WatchMovieWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -15,7 +15,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using WatchMovie.Models;
@@ -116,6 +118,11 @@ namespace WatchMovieWeb.Areas.Identity.Pages.Account
 
             public string? PhoneNumber { get; set; }
 
+            public string? Role { get; set; }
+
+            [ValidateNever]
+            public IEnumerable<SelectListItem> RoleList { get; set; }
+
 
         }
 
@@ -132,6 +139,15 @@ namespace WatchMovieWeb.Areas.Identity.Pages.Account
             }
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            Input = new InputModel()
+            {
+                RoleList = _roleManager.Roles.Select(x => x.Name).Select(i => new SelectListItem
+                {
+                    Text = i,
+                    Value = i
+                })
+            };
         }
 
         public async Task<IActionResult> OnPostAsync(string? returnUrl = null)
