e2bafd4aa1b6046ddab2e1f256c54e0a75c0728c
Bibek Parajuli <parajulibbek11@gmail.com>
2023-04-20 20:49:10 +0545

    createOrderHeaderAndOrderDetailModelCompleted

--- WatchMovirDataAccess/Data/ApplicationDbContext.cs
+++ WatchMovirDataAccess/Data/ApplicationDbContext.cs
@@ -22,6 +22,8 @@ namespace WatchMovieWeb.DataAccess
         public DbSet<Company> Companies { get; set; }
 
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
 
     }
 }
