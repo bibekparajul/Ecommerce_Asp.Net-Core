4e0ba5e3233eca032cd6e57c2c0954939d625c9d
Bibek Parajuli <parajulibbek11@gmail.com>
2023-04-20 21:40:17 +0545

    summaryGetActionMethodCompleted

--- WatchMovie.Models/ViewModels/ShoppingCartVM.cs
+++ WatchMovie.Models/ViewModels/ShoppingCartVM.cs
@@ -10,6 +10,6 @@ namespace WatchMovie.Models.ViewModels
     {
         public IEnumerable<ShoppingCart> ListCart { get; set; }
 
-        public double CartTotal  { get; set; }
+        public OrderHeader OrderHeader { get; set; }  
     }
 }
