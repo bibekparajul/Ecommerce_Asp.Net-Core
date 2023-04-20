7e7f1025ab73f49482ef0350179aa35ea6d44a84
Bibek Parajuli <parajulibbek11@gmail.com>
2023-04-20 19:25:09 +0545

    cartTotalCompleted

--- WatchMovie.Models/ViewModels/ShoppingCartVM.cs
+++ WatchMovie.Models/ViewModels/ShoppingCartVM.cs
@@ -9,5 +9,7 @@ namespace WatchMovie.Models.ViewModels
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ListCart { get; set; }
+
+        public double CartTotal  { get; set; }
     }
 }
