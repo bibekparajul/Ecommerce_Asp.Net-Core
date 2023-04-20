a2fbaf5982667d427cd189e28c906d9d70905305
Bibek Parajuli <parajulibbek11@gmail.com>
2023-04-20 20:15:14 +0545

    cartSummaryUiCompleted

--- WatchMovieWeb/Areas/Customer/Controllers/CartController.cs
+++ WatchMovieWeb/Areas/Customer/Controllers/CartController.cs
@@ -41,8 +41,28 @@ namespace WatchMovieWeb.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        public IActionResult Summary()
+        {
+            //var claimsIdentity = (ClaimsIdentity)User.Identity;
+            //var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
-       public IActionResult Plus(int cartId)
+            //ShoppingCartVM = new ShoppingCartVM()
+            //{
+            //    ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties:
+            //    "Product")
+            //};
+
+            //foreach (var cart in ShoppingCartVM.ListCart)
+            //{
+            //    cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price,
+            //        cart.Product.Price50, cart.Product.Price100);
+            //    ShoppingCartVM.CartTotal += (cart.Price * cart.Count);
+            //}
+            return View();
+        }
+
+
+        public IActionResult Plus(int cartId)
         {
             var cart = _unitOfWork.ShoppingCart.GetFirstorDefault(u => u.Id == cartId);
             _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
