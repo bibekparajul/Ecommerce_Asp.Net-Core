e7c49aedfc0875a78e17c748d954acbb01782887
Bibek Parajuli <parajulibbek11@gmail.com>
2023-04-20 20:06:39 +0545

    updatingTheQuantityInShoppingCart

--- WatchMovieWeb/Areas/Customer/Controllers/CartController.cs
+++ WatchMovieWeb/Areas/Customer/Controllers/CartController.cs
@@ -41,6 +41,48 @@ namespace WatchMovieWeb.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+
+       public IActionResult Plus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstorDefault(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            _unitOfWork.Save(); 
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult Minus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstorDefault(u => u.Id == cartId);
+            if (cart.Count <= 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+
+            }
+            else
+            {
+            _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+
+            }
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult Remove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstorDefault(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+
+
+
         private double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100)
         {
             if (quantity <= 50)
